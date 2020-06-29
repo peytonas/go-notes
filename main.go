0803ef0103c4670ea60c504c363c72066acb09a5
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-06-29 15:26:05 -0600

    tests update

--- main.go
+++ main.go
@@ -200,4 +200,179 @@ func main() {
 	// default:
 	// 	fmt.Println("I hate sports")
 	// }
+
+	//REVIEW "i" is the index where we are, "v" is the value at index "i" from array "x" (i.e. for each index "i" within array "x", do something)
+	// x := [5]int{1, 2, 3, 4, 5}
+	// for i, v := range x {
+	// 	fmt.Println(i, v)
+	// }
+	// fmt.Printf("%T\n", x)
+
+	//REVIEW slices work exactly the same as arrays, just don't specify length when value is declared
+	// x := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// for i, v := range x {
+	// 	fmt.Println(i, v)
+	// }
+	// fmt.Printf("%T\n", x)
+
+	//REVIEW to create new slices from an array, declare new slice variable and define the range you want from within the array("x")
+	// x := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+	// var y []int = x[0:5]
+	// var z []int = x[5:10]
+	// var a []int = x[2:7]
+	// var b []int = x[1:6]
+	// fmt.Printf("%v\n%v\n%v\n%v\n", y, z, a, b)
+	// fmt.Printf("%T\n", x)
+
+	//REVIEW append redefines a previously declared array ("x") with a new value pushed onto its end (52)
+	// x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	// x = append(x, 52)
+	// fmt.Println(x)
+
+	//REVIEW append values from "x" up to position 3, AND values from position 6 to the end, then re-map array "x"
+	// x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	// x = append(x[:3], x[6:]...)
+	// fmt.Println(x)
+
+	//REVIEW "make" creates a slice of type string(1st arg) with a length of 5(2nd arg) and a capacity of 5(3rd arg)
+	// x := make([]string, 5, 5)
+	// x = []string{`Alabama`, `Washington`, `California`, `Hawaii`, `Florida`} //this is a composite literal
+	// fmt.Println(x)
+	// fmt.Println(len(x))
+	// fmt.Println(cap(x))
+
+	// for i := 0; i < len(x); i++ {
+	// 	fmt.Println(i, x[i])
+	// }
+
+	//REVIEW this creates a map where each key is a string, and each value for that key is a string
+	// x := map[string][]string{
+	// 	"bond_james":      []string{"Martinis", "Women", "Shaken, not stirred"},
+	// 	"no_dr":           []string{`being evil`, `ice cream`, `sunsets`},
+	// 	"moneypenny_miss": []string{`James Bond`, `Literature`, `Computer Science`},
+	// }
+
+	//REVIEW for each key("k") within each value("v") within map "x", print the key; then, for each value within each key, print the value.
+	// for k, v := range x {
+	// 	fmt.Println("This is for", k)
+	// 	for i, v2 := range v {
+	// 		fmt.Println("\t", i, v2)
+	// 	}
+	// }
+
+	//REVIEW this is exactly the same as above except line 273 adds a new record to the map by declaring the key, and then defining its values as a slice of strings, with the values in that slice
+	// x := map[string][]string{
+	// 	"bond_james":      []string{"Martinis", "Women", "Shaken, not stirred"},
+	// 	"no_dr":           []string{`being evil`, `ice cream`, `sunsets`},
+	// 	"moneypenny_miss": []string{`James Bond`, `Literature`, `Computer Science`},
+	// }
+
+	// x["Q"] = []string{`Tech`, `Scotch`, `Cars`} // adds record "Q" to map x
+
+	// for k, v := range x {
+	// 	fmt.Println("This is for", k)
+	// 	for i, v2 := range v {
+	// 		fmt.Println("\t", i, v2)
+	// 	}
+	// }
+
+	//REVIEW also exactly the same as above, but line 290 deletes a record from a map
+	// x := map[string][]string{
+	// 	"bond_james":      []string{"Martinis", "Women", "Shaken, not stirred"},
+	// 	"no_dr":           []string{`being evil`, `ice cream`, `sunsets`},
+	// 	"moneypenny_miss": []string{`James Bond`, `Literature`, `Computer Science`},
+	// }
+
+	// x["Q"] = []string{`Tech`, `Scotch`, `Cars`}
+
+	// delete(x, `no_dr`) //deletes from map "x"(arg 1) the specified record(arg 2)
+
+	// for k, v := range x {
+	// 	fmt.Println("This is for", k)
+	// 	for i, v2 := range v {
+	// 		fmt.Println("\t", i, v2)
+	// 	}
+	// }
+
+	//REVIEW defines type Person with its values
+	// type Person struct {
+	// 	firstName    string
+	// 	lastName     string
+	// 	faveIceCream []string
+	// }
+
+	//REVIEW creates types of Person
+	// person1 := Person{"Peyton", "Sonnefeld", []string{"Half Baked", "Strawberry", "Vanilla"}}
+	// person2 := Person{"Chelsea", "Dunn", []string{"Vanilla", "Chocolate"}}
+
+	//REVIEW creates a map containing type Person, and declares the values (key=lastName, value=person1/person2)
+	// m := map[string]Person{
+	// 	person1.lastName: person1,
+	// 	person2.lastName: person2,
+	// }
+
+	// fmt.Println(m)
+
+	//REVIEW for each type in "m", print that type and its value
+	// for _, v := range m {
+	// 	fmt.Println(v.firstName, v.lastName)
+	// 	//REVIEW for each value in faveIceCream, print the index and its value
+	// 	for i, val := range v.faveIceCream {
+	// 		fmt.Println(i, val)
+	// 	}
+	// 	fmt.Println("---------")
+	// }
+
+	//REVIEW declares Vehicle struct as an underlying struct of type Sedan and Truck
+	// type Vehicle struct {
+	// 	doors int
+	// 	color string
+	// }
+
+	// type Truck struct {
+	// 	Vehicle
+	// 	fourWheel bool
+	// }
+
+	// type Sedan struct {
+	// 	Vehicle
+	// 	luxury bool
+	// }
+
+	// f150 := Truck{
+	// 	Vehicle: Vehicle{
+	// 		doors: 2,
+	// 		color: "black"},
+	// 	fourWheel: true}
+	// taurus := Sedan{
+	// 	Vehicle: Vehicle{
+	// 		doors: 4,
+	// 		color: "blue"},
+	// 	luxury: true}
+
+	// fmt.Println(f150)
+	// fmt.Println(taurus)
+	// fmt.Println(f150.color)
+	// fmt.Println(taurus.doors)
+
+	//REVIEW anonymous struct declared, same as structs above without a declared type; struct is given values, then types of the anonymous struct are declared
+	// s := struct {
+	// 	name      string
+	// 	friends   map[string]int
+	// 	favDrinks []string
+	// }{
+	// 	name: "Peyton",
+	// 	friends: map[string]int{
+	// 		"Chelsea": 444,
+	// 		"Tom":     555,
+	// 	},
+	// 	favDrinks: []string{
+	// 		"Monster",
+	// 		"Water",
+	// 	},
+	// }
+
+	// fmt.Println(s.name)
+	// fmt.Println(s.friends)
+
 }
